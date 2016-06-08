df5e6ff01bc953ab5abb9d72d86af169ea3259c4
GrCOTE7 <GrCOTE7@GMail.com>
2016-06-08 03:11:08 +0200

    Ajout Salut en php & lien marcgg

--- index.php
+++ index.php
@@ -1,19 +1,24 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
-    <meta charset="UTF-8">
-    <title>Tuto Git-GitHub</title>
+  <meta charset="UTF-8">
+  <title>Tuto Git-GitHub</title>
 </head>
 
 <body>
 
 <h1>Juste pour apprendre Git & GitHub</h1>
-<p>Tuto de marcgg</p>
+<h2>Tuto de <a target="_blank" href="https://github.com/GrCOTE7/marcgg_git_and_github">marcgg</a></h2>
+
 <section>
-    <ul>
-        <li>Bases de Git</li>
-        <li>Github</li>
-    </ul>
+  <ul>
+    <li>Bases de Git</li>
+    <li>Github</li>
+  </ul>
+  <hr/>
+  <?= 'Salut' ?>
 </section>
+
 </body>
 </html>
